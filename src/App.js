e5b1d52af92a2abb79d75b46b22228f280a76dc0
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2022-12-31 15:50:01 +0100

    introducing state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./style.css";
 
 const initialFacts = [
@@ -34,6 +35,22 @@ const initialFacts = [
   },
 ];
 
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <span style={{ fontSize: "40px" }}>{count}</span>
+      <button
+        className='btn btn-large'
+        onClick={() => setCount(count => count + 1)}
+      >
+        +1
+      </button>
+    </div>
+  );
+}
+
 function App() {
   const appTitle = "Today I Learned";
 
@@ -49,6 +66,7 @@ function App() {
         <button className='btn btn-large btn-open'>Share a fact</button>
       </header>
 
+      <Counter />
       <NewFactForm />
 
       <main className='main'>
