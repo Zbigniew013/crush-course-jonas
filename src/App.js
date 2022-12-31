64c882b6284b6ce3c86c387076c26b1d3a98ef70
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2022-12-31 14:49:34 +0100

    rendering the list of categories

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import "./style.css";
 
-const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
-];
-
 const initialFacts = [
   {
     id: 1,
@@ -74,8 +63,37 @@ function NewFactForm() {
   return <form className='fact-form'>Fact Form</form>;
 }
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 function CategoryFilter() {
-  return <aside>Category Filter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li className='category'>
+          <button className='btn btn-all-categories'>All</button>
+        </li>
+        {CATEGORIES.map(cat => (
+          <li key={cat.name} className='category'>
+            <button
+              className='btn btn-category'
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 function FactList() {
@@ -86,7 +104,7 @@ function FactList() {
     <section>
       <ul className='facts-list'>
         {facts.map(fact => (
-          <Fact key={fact.id} fact={fact} test='23' />
+          <Fact key={fact.id} fact={fact} />
         ))}
       </ul>
       <p>They are {facts.length} facts in the database. Add your own! </p>
