5579b272873807091edeb6012ad781cdcb372ab5
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-01-03 13:41:30 +0100

    ready for priduction

--- src/App.js
+++ src/App.js
@@ -148,7 +148,7 @@ function isValidHttpUrl(string) {
 
 function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState("");
-  const [source, setSource] = useState("http://example.com");
+  const [source, setSource] = useState("");
   const [category, setCategory] = useState("");
   const [isUploading, setIsUploading] = useState(false);
   const textLength = text.length;
@@ -279,6 +279,8 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     setIsUpdating(true);
@@ -300,6 +302,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className='fact'>
       <p>
+        {isDisputed ? <span className='disputed'>[DISPUTED]</span> : null}
         {fact.text}
         <a
           className='source'
